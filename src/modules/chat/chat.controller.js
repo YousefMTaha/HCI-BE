d09a9418219f7c9ee188c89c67d0d64469d23aec
Yousef Taha <tyousef262@gmail.com>
2024-12-16 00:24:13 +0200

    error handling

--- src/modules/chat/chat.controller.js
+++ src/modules/chat/chat.controller.js
@@ -1,41 +1,45 @@
 import * as socketService from "./chat.service.js";
 
 function configSocket(io) {
-  io.on("connection", (socket) => {
-    console.log("connected");
-    console.log(socket.id);
+  try {
+    io.on("connection", (socket) => {
+      console.log("connected");
+      console.log(socket.id);
 
-    socket.on("updateSocketId", async (token) => {
-      console.log("updatedSocketID");
+      socket.on("updateSocketId", async (token) => {
+        console.log("updatedSocketID");
 
-      const user = await socketService.checkToken(token);
-      await socketService.setNewSocketId(user._id, socket.id);
-    });
+        const user = await socketService.checkToken(token);
+        await socketService.setNewSocketId(user._id, socket.id);
+      });
 
-    socket.on("addMessage", async (data) => {
-      console.log("addMsg");
-      const user = await socketService.checkToken(data.token);
+      socket.on("addMessage", async (data) => {
+        console.log("addMsg");
+        const user = await socketService.checkToken(data.token);
 
-      // console.log({ data });
+        // console.log({ data });
 
-      await socketService.addMsg(user, data);
+        await socketService.addMsg(user, data);
 
-      io.emit("newMessage", {
-        from: user._id,
-        to: data.to,
-        message: data.message,
+        io.emit("newMessage", {
+          from: user._id,
+          to: data.to,
+          message: data.message,
+        });
+      });
+
+      socket.on("getMessages", async (data) => {
+        // console.log("getMessages");
+        const user = await socketService.checkToken(data.token);
+        const msgs = await socketService.getAllMsgs(user._id, data.to);
+        // console.log(msgs);
+
+        socket.emit("retrieveMessages", { messages: msgs });
       });
     });
-
-    socket.on("getMessages", async (data) => {
-      // console.log("getMessages");
-      const user = await socketService.checkToken(data.token);
-      const msgs = await socketService.getAllMsgs(user._id, data.to);
-      // console.log(msgs);
-      
-      socket.emit("retrieveMessages", { messages: msgs });
-    });
-  });
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export default configSocket;
